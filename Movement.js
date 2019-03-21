7a1827a5fb94add8192ea24dd008a1e0212e0574
Axelm3sh <axel.m3sh@gmail.com>
2019-03-20 20:24:11 -0700

    Update Movement.js

--- Movement.js
+++ Movement.js
@@ -3,11 +3,13 @@ var charOneSpr = document.querySelector("#SomeID2");
 
 var sqrGridSize = 8;//might not be used?
 
-var array_GridTiles = []; //empty array
+var array_GridTiles = []; //empty array, should be filled with tileData
 
+//Tile data to be paired with DOM element
 var tileData = {
     column: "",
-    row: ""
+    row: "",
+    gridID: "null" //this is the element ID
 };
 
 
@@ -27,6 +29,8 @@ function initializeGridSize()
 {
     //use query to search for all grid element frames? (ie: #Grid0-0 )
     var allTilesCSS = document.querySelectorAll("#GridTile");
+    
+    
     
     
 } 
