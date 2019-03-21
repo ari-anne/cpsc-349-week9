3b1e0ef7ca872901400d01a332e06de51b12d5f6
Axelm3sh <axel.m3sh@gmail.com>
2019-03-20 20:34:09 -0700

    more updates on script and added to html

--- Movement.js
+++ Movement.js
@@ -1,6 +1,9 @@
 var charOneSpr = document.querySelector("#SomeID1");
 var charOneSpr = document.querySelector("#SomeID2");
 
+var C1;
+
+
 var sqrGridSize = 8;//might not be used?
 
 var array_GridTiles = []; //empty array, should be filled with tileData
@@ -18,6 +21,7 @@ window.onload = function()
 {
     alert("Hello, welcome to the grid board!");
     
+    myGamePiece = new component(30, 30, "red", 10, 120);
     
 };
 
@@ -27,10 +31,32 @@ window.onload = function()
 //Parameter is the square grid size
 function initializeGridSize()
 {
+    var boardElement = document.querySelector(".board");
+    
+    var innerHtmlOfTile = "<div class=\"tileBackground\">Tile!</div>";
     //use query to search for all grid element frames? (ie: #Grid0-0 )
-    var allTilesCSS = document.querySelectorAll("#GridTile");
+    //var allTilesCSS = document.querySelectorAll("#GridTile");
+    for(i = 0; i < (sqrGridSize*sqrGridSize); i++)
+        {
+            
+        }
     
     
+    
+    //go thru all found GridTiles, rename their ID to match the grid size
     
     
 } 
+
+
+
+
+function MoveUp(char)
+{
+    
+}
+
+function checkMovementTo(theNewTile)
+{
+    
+}
